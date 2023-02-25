393564aac625ae35c43d0132b1a572682a088717
Alison <alison821@gmail.com>
2023-02-25 19:38:16 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-project/'
+    ? '/E-commerce-website/'
     : '/',
   transpileDependencies: true
 })
